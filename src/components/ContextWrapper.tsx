b37678d5545e2412bc5bb10db8dd3caa3dd01e12
RomanSh <romashmarov82@gmail.com>
2023-12-15 05:23:19 +0300

    global context updated

--- src/components/ContextWrapper.tsx
+++ src/components/ContextWrapper.tsx
@@ -1,23 +1,49 @@
 import GlobalStyles, { styleTheme } from '../styledComponents/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useThemeToggle from '../hooks/useThemeToggle';
 import GlobalContextInt from '../interfaces/globalContextInt';
+import useFilterChange from '../hooks/useFilterChange';
 
+const mobileWidthCondition = "(max-width: 768px)";
 
 const AppContext = React.createContext<GlobalContextInt | null>(null);
 
 export default function ContextWrapper({ children }: { children: React.JSX.Element}) {
 
+	// defining and listening for device orientation
+	const [isMobile, setIsMobile] = useState(
+		window.matchMedia(mobileWidthCondition).matches
+	)
+	
+	useEffect(() => {
+		function checkCondition() {
+			setIsMobile(window.matchMedia(mobileWidthCondition).matches);
+		}
+
+		window.matchMedia(mobileWidthCondition)
+			.addEventListener('change', checkCondition);
+
+		return () => {
+			window.matchMedia(mobileWidthCondition)
+				.removeEventListener('change', checkCondition)
+		}
+	});
+
+	// hooks
     const [theme, toggleTheme] = useThemeToggle();
 
-	//global context
+	const [filterStatus, handleFilterChange] = useFilterChange();
 
     return (
 		<AppContext.Provider value={{
 			theme,
 			toggleTheme,
+			filterStatus,
+			handleFilterChange,
+			orientation: isMobile ? "mobile" : "desktop",
+			
 		}}>
 			<ThemeProvider theme={theme === 'dark' ? styleTheme.dark : styleTheme.light}>
 				<GlobalStyles $isDark={theme === 'dark' ? true : false}/>
--- src/interfaces/globalContextInt.ts
+++ src/interfaces/globalContextInt.ts
@@ -1,6 +1,24 @@
-type VoidFunc = () => void;
+type VoidFuncType = () => void;
+
+export type FilterStatusType = {
+    all: boolean,
+    draft: boolean,
+    pending: boolean,
+    paid: boolean,
+};
+
+export interface CustomHTMLButtonElement extends HTMLButtonElement {
+    filterType: 'pending' | 'draft' | 'paid';
+    $checked: boolean;
+};
+
+type FilterHandleType = (e: React.MouseEvent<CustomHTMLButtonElement>) => void;
+
 
 export default interface GlobalContextInt {
     theme: "light" | "dark";
-    toggleTheme: VoidFunc;
+    toggleTheme: VoidFuncType;
+    filterStatus: FilterStatusType;
+    handleFilterChange: FilterHandleType;
+    orientation: "mobile" | "desktop";
 }
