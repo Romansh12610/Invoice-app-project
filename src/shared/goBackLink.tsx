9ce4a903a3bee867da52d72dd20a4ec9b4dce34e
RomanSh <romashmarov82@gmail.com>
2023-12-21 05:23:34 +0300

    extract backLink component

---
+++ src/shared/goBackLink.tsx
@@ -0,0 +1,47 @@
+import { styled, useTheme } from "styled-components";
+import { Link } from "react-router-dom";
+import rem from "../utilities/pxIntoRem";
+import Icon from "../Icon/Icon";
+
+interface goBackLinkInterface {
+    to: string;
+}
+
+export const GoBackLinkWrapper = styled(Link)`
+    font-size: ${rem(16)};
+    font-weight: 500;
+    color: ${({theme}) => theme.textColor};
+    display: block;
+    width: max-content;
+    margin-block: ${rem(20)};
+    border: 2px solid transparent;
+    border-radius: ${rem(15)};
+    padding: 0.5rem;
+    transition: border-color 250ms ease-in-out;
+
+    &:hover {
+        border: 2px solid ${({theme}) => theme.general.purple};
+    }
+`;
+
+export const GoBackLinkText = styled.span`
+    padding-left: ${rem(10)};
+`;
+
+const GoBackLink = (props: goBackLinkInterface) => {
+
+    const colorTheme = useTheme();
+
+    return (
+        <GoBackLinkWrapper to={props.to}>
+            <Icon 
+                name='arrow-left'
+                size={11}
+                color={colorTheme.general.purple}
+            />
+            <GoBackLinkText>Go back</GoBackLinkText>
+        </GoBackLinkWrapper>
+    );
+}
+
+export default GoBackLink;
