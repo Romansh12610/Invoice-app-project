262ca771b05825f0e797f9f282d263e1a5d3590f
RomanSh <romashmarov82@gmail.com>
2023-12-22 02:40:11 +0300

    update modal logic

--- src/components/FormInvoice.tsx
+++ src/components/FormInvoice.tsx
@@ -1,19 +1,58 @@
 import { MainWrapper, Form, Input, Label, Title, FieldSet, Legend, Backdrop, InputLabelWrapper } from "../styledComponents/FormInvoiceStyled";
 import { useGlobalContext } from "./ContextWrapper";
 import { useParams } from "react-router-dom";
+import {createPortal} from 'react-dom';
+import GoBackLink from "../shared/goBackLink";
+import { useEffect, useRef } from 'react';
+import { focusTrapKeyDown, focusTrapKeyUp, keySetType, closeModalIfOutsideClick } from "../utilities/modalUtilities";
+
 
 const FormController = () => {
 
+    // refs for Backdrop and Form
+    const modalRef = useRef(null);
+    const backdropRef = useRef(null);
+
     const URLparams = useParams();
-    const { globalState } = useGlobalContext();
+    const { globalState, dispatchAction } = useGlobalContext();
     const { isInvoiceEdited } = globalState;
+
+    useEffect(() => {
+        // prevent scroll when modal is active
+        document.body.style.overflow = 'hidden';
+        // focus trap
+        const keySet: keySetType = new Set();
+        const closeFormCallback = () => dispatchAction({ type: 'closeForm' });
+
+        const onKeyDown = (e: KeyboardEvent) => focusTrapKeyDown(e, modalRef, closeFormCallback, keySet);
+        const onKeyUp = (e: KeyboardEvent) => focusTrapKeyUp(e, keySet);
+
+        document.addEventListener('keydown', onKeyDown);
+        document.addEventListener('keyup', onKeyUp);
+
+        return () => {
+            // enable scrolling after modal closing
+            document.body.style.overflow = 'unset';
+            document.removeEventListener('keydown', onKeyDown);
+            document.removeEventListener('keyup', onKeyUp);
+        }
+    }, []);
     
-    return (
+    const controller = (
         <>
-            <Backdrop />
-            <MainWrapper>
+            <Backdrop 
+                ref={backdropRef}
+            />
+            <MainWrapper
+                aria-modal={true}
+                aria-label="Invoice form"
+                tabIndex={-1}
+                role="dialog"
+                ref={modalRef}
+            >
+                <GoBackLink to="/" />
                 <Title>
-                    {isInvoiceEdited ? 'New Invoice' : `Edit &#35;${URLparams.invoiceId}`}
+                    {isInvoiceEdited == false ? 'New Invoice' : `Edit &#35;${URLparams.invoiceId}`}
                 </Title>
                 <Form>
                     <FieldSet $name='user-address'>
@@ -26,7 +65,9 @@ const FormController = () => {
                 </Form>
             </MainWrapper>
         </>
-    )
-}
+    );
+
+    return createPortal(controller, document.body);
+};
 
 export default FormController;
---
+++ src/utilities/modalUtilities.ts
@@ -0,0 +1,69 @@
+import React from 'react';
+
+// types
+type closeCallbackType = () => void;
+
+type closeModalType = (e: MouseEvent, modalRef: React.RefObject<HTMLElement>, closeCallback: closeCallbackType) => void;
+
+export type keySetType = Set<'Tab' | 'Shift'>; 
+
+type FocusTrapDownType = (e: KeyboardEvent, modalRef: React.RefObject<HTMLFormElement | HTMLDivElement>, closeCallback: closeCallbackType, keySet: keySetType) => void;
+
+type FocusTrapUpType = (e: KeyboardEvent, keySet: keySetType) => void;
+
+// functions
+export const closeModalIfOutsideClick: closeModalType = (e, modalRef, closeCallback) => {
+    const { currentTarget } = e;
+    if (currentTarget !== modalRef.current) {
+       closeCallback();
+    } 
+    else {
+        return;
+    }
+};
+
+
+export const focusTrapKeyDown: FocusTrapDownType = (e, modalRef, closeCallback, keySet: keySetType) => {
+    // focus on form
+    if (e.key === 'Escape') {
+        closeCallback();
+        return;
+    } else if (e.key !== 'Tab' && e.key !== 'Shift') return;
+
+    // add key to set to track it
+    keySet.add(e.key);
+
+    const focusableElementsList: NodeListOf<HTMLInputElement | HTMLAnchorElement | HTMLButtonElement> = modalRef.current.querySelectorAll('a, input, button');
+
+    const firstItem = focusableElementsList[0];
+    const listLength = focusableElementsList.length;
+    const lastItem = focusableElementsList[listLength - 1];
+
+    if (document.activeElement === firstItem && e.key === 'Tab' && keySet.has('Shift')) {
+        lastItem.focus();
+        lastItem.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'center'
+        });
+    }
+
+    else if (document.activeElement === lastItem && e.key === 'Tab') {
+        firstItem.focus();
+        firstItem.scrollIntoView(
+            {
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'center'
+            }
+        )
+    }
+}
+
+export const focusTrapKeyUp: FocusTrapUpType = (e, keySet) => {
+    if (e.key !== 'Tab' && e.key !== 'Shift') {
+        return;
+    } else {
+        keySet.delete(e.key);
+    }
+}
