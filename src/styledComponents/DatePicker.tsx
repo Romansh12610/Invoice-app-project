f233a088da09624fe090d63930b0d097043415b0
RomanSh <romashmarov82@gmail.com>
2023-12-28 05:25:27 +0300

    datepicker logic updated

--- src/styledComponents/DatePicker.tsx
+++ src/styledComponents/DatePicker.tsx
@@ -4,15 +4,14 @@ import styled from 'styled-components';
 import { defaultInput } from './FormInvoiceStyled';
 import Icon from '../Icon/Icon';
 import convertDateOutput from '../utilities/convertDateOutput';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { useGlobalContext } from '../components/ContextWrapper';
 import { useTheme } from 'styled-components';
+import rem from '../utilities/pxIntoRem';
 
-export const StyledDatePicker = styled.button`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+export const StyledDatePicker = styled.input`
     ${defaultInput};
+    width: 100%;
 
     &:disabled {
         cursor: not-allowed;
@@ -22,36 +21,55 @@ export const StyledDatePicker = styled.button`
 
 interface CustomInputProps {
     isDisabled: boolean;
-    value?: string;
-    onClick?: (e?: React.MouseEvent) => void;
     color: string;
+    value?: string;
+    onClick?: (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    onFocus?: (e: React.FocusEvent<HTMLButtonElement>) => void;
+    onBlur?: (e: React.FocusEvent<HTMLButtonElement>) => void;
+    onChange?: (e: React.ChangeEvent<HTMLButtonElement>, date: Date | null) => void;
+    ref?: React.RefObject<HTMLInputElement>;
 }
 
-export const CustomInput: React.FC<CustomInputProps> = ({ isDisabled, value, onClick, color }) => (
-    <StyledDatePicker
-        disabled={isDisabled}
-        value={value}
-        onClick={onClick}
-    >
-        {convertDateOutput(value)}
-        <Icon 
-            name='calendar'
-            size={12}
-            color={color}
+export const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(({ isDisabled, value, color, onBlur, onFocus, onChange }, ref) => (
+    <div style={{
+        position: 'relative',
+        display: 'flex',
+        flex: '1',
+        minWidth: `${rem(140)}`
+    }}>
+        <StyledDatePicker
+            disabled={isDisabled}
+            value={convertDateOutput(value)}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            onChange={(e) => onChange(e, new Date())}
+            ref={ref}
+            readOnly
         />
-    </StyledDatePicker>
-);
-
+        <Icon 
+            color={color}
+            size={16}
+            name='calendar'
+            customStyle={{
+                position: 'absolute',
+                right: '5%',
+                top: '50%',
+                transform: 'translateY(-50%)'
+            }}
+        />
+    </div>
+));
 
 const DatePicker = () => {
 
     const { globalState, newInvoice, handleInvoiceChange } = useGlobalContext();
     const colorTheme = useTheme();
 
+
     return (
         <ReactDatePicker 
             selected={new Date(newInvoice.createdAt)}
-            onChange={(date) => handleInvoiceChange(null, 'date', date)}
+            onChange={(date: Date | null) => handleInvoiceChange(false, 'date', date)}
             minDate={new Date()}
             customInput={
             <CustomInput 
