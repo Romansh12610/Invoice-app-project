73caca99a02fdca7928e0320b92b6db6cf98218e
Roman Shmarov <romashmarov82@gmail.com>
2024-01-12 03:11:22 +0300

    form validation utility function added

--- src/utilities/formValidation.ts
+++ src/utilities/formValidation.ts
@@ -1,20 +1,10 @@
 
-type ValidateForm = (formElement: EventTarget & HTMLFormElement) => boolean;
+type ValidateForm = (formRef: React.RefObject<HTMLFormElement>) => boolean;
 
 const validateForm: ValidateForm = (formEl) => {
-    /* const inputCollection: NodeListOf<HTMLInputElement> = formEl.querySelectorAll('input[required]');
+    const form = formEl.current;
 
-    let isFormValid = true;
-
-    for (const input of inputCollection) {
-        if (!input.validity.valid) {
-            isFormValid = false;
-        }
-    };
-
-    return isFormValid; */
-
-    return formEl.reportValidity();
+    return form.reportValidity();
 };
 
 export default validateForm;
