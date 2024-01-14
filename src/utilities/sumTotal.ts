d8504fa1e51fefd761c7d7edd597adb9c7f332a4
Roman Shmarov <romashmarov82@gmail.com>
2024-01-14 04:40:12 +0300

    sum total utility created
    this function gets sum from all item.total props of 1 invoice

---
+++ src/utilities/sumTotal.ts
@@ -0,0 +1,8 @@
+import { InitialItemInterface } from "../interfaces/invoiceTypes";
+
+const sumTotal = (itemsArray: InitialItemInterface[]) => {
+    const arrayWithTotalSums = itemsArray.map(el => el.total);
+    return arrayWithTotalSums.reduce((acc, curr) => acc + curr, 0); 
+};
+
+export default sumTotal;
