a790719a08dd334a15752888ea66282d362d51e9
Roman Shmarov <romashmarov82@gmail.com>
2024-01-15 20:44:58 +0300

    datePicker performance fix

--- src/styledComponents/DatePicker.tsx
+++ src/styledComponents/DatePicker.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { defaultInput } from './FormInvoiceStyled';
 import Icon from '../Icon/Icon';
 import convertDateOutput from '../utilities/convertDateOutput';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useMemo } from 'react';
 import { useGlobalContext } from '../components/ContextWrapper';
 import { useTheme } from 'styled-components';
 import rem from '../utilities/pxIntoRem';
@@ -30,35 +30,42 @@ interface CustomInputProps {
     ref?: React.RefObject<HTMLInputElement>;
 }
 
-export const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(({ isDisabled, value, color, onBlur, onFocus, onChange }, ref) => (
-    <div style={{
-        position: 'relative',
-        display: 'flex',
-        flex: '1',
-        minWidth: `${rem(140)}`
-    }}>
-        <StyledDatePicker
-            disabled={isDisabled}
-            value={convertDateOutput(value)}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            onChange={onChange}
-            ref={ref}
-            readOnly
-        />
-        <Icon 
-            color={color}
-            size={16}
-            name='calendar'
-            customStyle={{
-                position: 'absolute',
-                right: '5%',
-                top: '50%',
-                transform: 'translateY(-50%)'
-            }}
-        />
-    </div>
-));
+export const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(({ isDisabled, value, color, onBlur, onFocus, onChange }, ref) => {
+
+    const dateOutput = useMemo(() => {
+        return convertDateOutput(value);
+    }, [value]);
+
+    return (
+        <div style={{
+            position: 'relative',
+            display: 'flex',
+            flex: '1',
+            minWidth: `${rem(140)}`
+        }}>
+            <StyledDatePicker
+                disabled={isDisabled}
+                value={dateOutput}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                onChange={onChange}
+                ref={ref}
+                readOnly
+            />
+            <Icon 
+                color={color}
+                size={16}
+                name='calendar'
+                customStyle={{
+                    position: 'absolute',
+                    right: '5%',
+                    top: '50%',
+                    transform: 'translateY(-50%)'
+                }}
+            />
+        </div>
+    )
+});
 
 const DatePicker = () => {
 
