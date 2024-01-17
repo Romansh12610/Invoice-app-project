0aa5392659c58762e9aff135e6e718d763dfbbeb
Roman Shmarov <romashmarov82@gmail.com>
2024-01-17 05:19:23 +0300

    types of reducer actions corrected

--- src/interfaces/reducerTypes.ts
+++ src/interfaces/reducerTypes.ts
@@ -2,6 +2,6 @@ import { InitialInvoiceInterface } from "./invoiceTypes";
 import { FilterStatusType } from "./filterTypes";
 
 export default interface ReducerActions {
-    type: 'filter' | 'openForm' | 'closeForm' | 'addInvoice' | 'addDraft' | 'saveChanges' | 'discardChanges' | 'closeModal' | 'deleteInvoice' | 'changeStatus';
+    type: 'filter' | 'openForm' | 'closeForm' | 'addInvoice' | 'addDraft' | 'saveChanges' | 'discardChanges' | 'closeModal' | 'openModal' | 'deleteInvoice' | 'changeStatus' | 'resetDeletedInvoice';
     payload?: InitialInvoiceInterface | FilterStatusType | string;
 };
